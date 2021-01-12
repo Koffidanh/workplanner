2aea8ea744ab36852e51d8a9bccb9ea32b80eff6
KoffiDanhounsrou <koffi.danh@gmail.com>
2021-01-12 15:30:19 -0700

    testing clone

--- script.js
+++ script.js
@@ -2,4 +2,22 @@
 $("#currentDay").text((moment().format('MMMM Do YYYY, h:mm:ss a')));
 //Create a legends
 $("#legend").text = "Legends:"
-// Create time from 11:59 am to 11:59 pm
+// Create time from 08:00 am to 05:00 pm
+$(document).ready(function () {
+    for (let i = 8; i <= 17; i++) {
+        console.log(i);
+        var newRow = $(".clonedRow:first").clone();
+        $(newRow).insertAfter(".clonedRow");
+        $("#hours").text(i);
+
+        // var newRowDescr = $(".cloneInput:first").clone();
+        // $(newRowDescr).insertAfter(".cloneInput");
+
+        // var newRowSaveBtn = $(".cloneBtn:first").clone();
+        // $(newRowDescr).insertAfter(".cloneBtn");
+    }
+});
+//Popover button
+// $(document).ready(function(){
+//     $('[data-toggle="popover"]').popover();   
+// });
