ebf59b57a088be105ba615c48fa291f2d402be55
KoffiDanhounsrou <koffi.danh@gmail.com>
2021-01-11 23:41:33 -0700

    date and time worked

---
+++ script.js
@@ -0,0 +1,5 @@
+// Set up a date and time
+$("#currentDay").text((moment().format('MMMM Do YYYY, h:mm:ss a')));
+//Create a legends
+$("#legend") = "Legends:"
+// Create time from 11:59 am to 11:59 pm
